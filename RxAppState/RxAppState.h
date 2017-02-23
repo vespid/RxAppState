b733cd69f6dd6f8ad33598f03a6efe72de76c422
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-02-23 18:03:29 +0900

    Add Cocoa Touch Framework (as xcodeproj|) and Cartfile

---
+++ RxAppState/RxAppState.h
@@ -0,0 +1,19 @@
+//
+//  RxAppState.h
+//  RxAppState
+//
+//  Created by mitsuyoshi.yamazaki on 2017/02/23.
+//  Copyright © 2017年 pixeldock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAppState.
+FOUNDATION_EXPORT double RxAppStateVersionNumber;
+
+//! Project version string for RxAppState.
+FOUNDATION_EXPORT const unsigned char RxAppStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAppState/PublicHeader.h>
+
+
